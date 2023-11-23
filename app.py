3dbe4c266218926e5fee9e3d2b2e2d4536b4c204
victorzllDev <joaovictor12200capa@gmail.com>
2023-11-23 19:26:22 -0300

    metodo POST para cria base da season

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, request, make_response, jsonify
 from firebase_admin import initialize_app, credentials, firestore
 from firebaseConfig import FirebaseConfig
 
@@ -10,11 +10,11 @@ db = firestore.client()
 app = Flask(__name__)
 
 
-@app.route("/seasons/", defaults={'season': 'none'})
-@app.route("/seasons/<string:season>", methods=['GET'])
+@app.route('/seasons/', defaults={'season': 'none'})
+@app.route('/seasons/<string:season>', methods=['GET'])
 def getSeasons(season):
     # Fetching data from the database.
-    docs = db.collection("seasons").stream()
+    docs = db.collection('seasons').stream()
     data = {}
     for doc in docs:
         data.update({doc.id: doc.to_dict()})
@@ -30,6 +30,21 @@ def getSeasons(season):
             return make_response(jsonify(data))
     else:
         return make_response(jsonify({'error': 'Unable to find data'}), 404)
+
+
+@app.route('/seasons', methods=['POST'])
+def postSeasons():
+    # check if the season name already exists in the database
+    nameSeason = str(request.json['season'])
+    docs = db.collection('seasons').stream()
+    for doc in docs:
+        if doc.id == nameSeason:
+            return make_response(jsonify({'error': 'This season already exists'}), 404)
+
+    # else if => creates normally in the database
+    db.collection("seasons").document(
+        nameSeason).set({'teams': [], 'matches': []})
+    return make_response(jsonify({'message': 'season created successfully'}), 201)
 
 
 if __name__ == '__main__':
