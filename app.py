eee5f8d884bc18482d7ae984660e6926533828ff
victorzllDev <joaovictor12200capa@gmail.com>
2023-11-25 21:27:11 -0300

    reajuste na rota seasons, metodo GET

--- app.py
+++ app.py
@@ -10,41 +10,32 @@ db = firestore.client()
 app = Flask(__name__)
 
 
-@app.route('/seasons/', defaults={'season': 'none'})
+@app.route('/seasons/', defaults={'season': ''})
 @app.route('/seasons/<string:season>', methods=['GET'])
 def getSeasons(season):
-    # Fetching data from the database.
-    docs = db.collection('seasons').stream()
-    data = {}
-    for doc in docs:
-        data.update({doc.id: doc.to_dict()})
+    # checks if any value passes in the route.
+    if season != '':
+        collections = db.collection("seasons").document(season).collections()
+        data = {}
+        for collection in collections:
+            collection_data = []
 
-    # check if there is a date, and verify if a parameter is present in the URL.
-    if data:
-        if season != 'none':
-            try:
-                return make_response(jsonify(data[season]))
-            except KeyError:
-                return make_response(jsonify({'error': 'Unable to find season data'}), 404)
-        else:
+            for doc in collection.stream():
+                collection_data.append(doc.to_dict())
+
+            data[collection.id] = collection_data
+        # returns the date of the season that came on the route
+        if data:
             return make_response(jsonify(data))
+        else:
+            return make_response(jsonify({'error': 'season does not exist'}), 404)
     else:
-        return make_response(jsonify({'error': 'Unable to find data'}), 404)
-
-
-@app.route('/seasons', methods=['POST'])
-def postSeasons():
-    # check if the season name already exists in the database
-    nameSeason = str(request.json['season'])
-    docs = db.collection('seasons').stream()
-    for doc in docs:
-        if doc.id == nameSeason:
-            return make_response(jsonify({'error': 'This season already exists'}), 404)
-
-    # else if => creates normally in the database
-    db.collection("seasons").document(
-        nameSeason).set({'teams': [], 'matches': []})
-    return make_response(jsonify({'message': 'season created successfully'}), 201)
+        # returns existing routes
+        docs = db.collection('seasons').stream()
+        data = []
+        for doc in docs:
+            data.append(doc.id)
+        return make_response(jsonify(data))
 
 
 if __name__ == '__main__':
